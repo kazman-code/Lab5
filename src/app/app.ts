35467ac9b44b3396ee6f1b2f0299dcccbe7f18a4
Mick_code <mickdonnellan2@gmail.com>
2026-03-08 19:24:20 +0000

    Update app.ts
    comments

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,40 @@
-import { Component, signal } from '@angular/core';//imports component and signal
-import { RouterOutlet } from '@angular/router';//imports router outlet
+//imports
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DataService } from './Services/data-service';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],//allows routing to work in this component
+  imports: [RouterOutlet],
   templateUrl: './app.html',//html file
   styleUrl: './app.css'//css file
 })
-export class App {
-  protected readonly title = signal('data-app');//reactive variable that stores app title using signal()
+ 
+export class App implements OnInit{
+  //stores student data gotten from api
+  students:any[]=[];
+  //variable that stores weather data from api
+  weatherData: any;
+  //injects dataservice so it can be used
+  constructor(private dataService:DataService){}
+  //runs when component is initialised
+  ngOnInit(): void {
+
+      //calls service method to get weather data
+    this.dataService.GetStudentData().subscribe((data) => {
+      this.students = data.students;//stores student data from api
+      console.log(this.students);//prints student data to console
+    });
+
+  //calls service method to get weather data
+    this.dataService.GetweatherData().subscribe((data) => {
+        this.weatherData = data;//stores weather api response
+        console.log(this.weatherData);//prints weather data to console
+
+      });
+
+
+
+}
 }
