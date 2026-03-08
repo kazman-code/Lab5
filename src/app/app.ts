970e2eea59d25519ea7aedcc9399931416675379
Mick_code <mickdonnellan2@gmail.com>
2026-03-08 13:54:40 +0000

    Update app.ts
    updates

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';//imports component and signal
+import { RouterOutlet } from '@angular/router';//imports router outlet
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet],//allows routing to work in this component
+  templateUrl: './app.html',//html file
+  styleUrl: './app.css'//css file
 })
 export class App {
-  protected readonly title = signal('data-app');
+  protected readonly title = signal('data-app');//reactive variable that stores app title using signal()
 }
